fd3ae530c41ec276181095772be4aa89f9118b81
Shreyaskali <83067948+Shreyaskali@users.noreply.github.com>
2024-04-15 08:31:56 +0530

    added aldep algo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 import uuid
 
 st.set_page_config(page_title="ALDEP")
@@ -59,7 +60,7 @@ for d in dept:
   val+=3
 ans = st.button("Submit")
 if ans:
-    st.write(data)
+    # st.write(data)
     flag = checkArea(F_l, F_w, data)
   
 st.header("Output") 
@@ -71,20 +72,6 @@ class Facility:
     self.height = height
     self.grid = [[None for _ in range(width)] for _ in range(height)]
 
-  def place_department(self, department, x, y):
-    # Check if there's enough space for the department
-    if x + department.width > self.width or y + department.height > self.height:
-      return False
-
-    # Check for overlaps with existing departments
-    for i in range(department.width):
-      for j in range(department.height):
-        if self.grid[y+j][x+i] is not None:
-          return False
-        else:
-          self.grid[y+j][x+i] = department  # logical error
-          return True
-
 # Department class to represent different functional areas
 class Department:
   def __init__(self, name, width, height, adjacency_list=[]):
@@ -94,52 +81,161 @@ class Department:
     self.adjacency_list = adjacency_list  # List of departments it needs to be close to
 
 # ALDEP function to generate a layout with visualization using Matplotlib
-def ALDEP(facility, departments):
-  # Shuffle the departments for random placement attempts
-  random.shuffle(departments)
+def ALDEP(facility, data):
+  initdept = random.randint(0, n-1)
+  prev = -1
+  deptlist = []
+  visited = [0 for i in range(n)]
+  deptlist.append(initdept)
+  visited[initdept] = 1
+  curr = initdept
 
-  # Try placing each department until successful
-  for department in departments:
-    placed = False
-    # Depending on layout precision the number of attempts can be formulated
-    for _ in range(100):  # Maximum of 100 attempts
-      x = random.randint(0, facility.width - department.width)
-      y = random.randint(0, facility.height - department.height)
-      if facility.place_department(department, x, y):
-        placed = True
-        break
+  while deptlist[-1] != prev:
+      prev = deptlist[-1]
+      adjacencylist = data[prev][2]
 
-    if not placed:
-      print(f"Failed to place department: {department.name}")
+      for i in range(len(adjacencylist)):
+          if not visited[i]:
+              visited[i] = 1
+              deptlist.append(i)
+              break
 
-  # Create a color map for departments
-  color_map = {'A': '#03071e', 'B': '#370617', 'C': '#6a040f','D':'#9d0208','E':'#d00000','F':'#dc2f02','G':'#e85d04','H':'#f48c06','I':'#faa307','J':'#ffba08'}
+  for i in range(n):
+      if not visited[i]:
+          deptlist.append(i)
+          visited[i] = 1
+          prev = -1
 
-  # Create the visualization using Matplotlib
-  fig, ax = plt.subplots()
+          while deptlist[-1] != prev:
+              prev = deptlist[-1]
+              adjacencylist = data[prev][2]
 
-  # Set cell size based on facility dimensions
-  cell_width = 1
-  cell_height = 1
+              for i in range(len(adjacencylist)):
+                  if not visited[i]:
+                      visited[i] = 1
+                      deptlist.append(i)
+                      break
+  print(deptlist) 
 
-  # Iterate through the grid and create rectangles for departments
-  for x in range(facility.height):
-    for y in range(facility.width):
-      department = facility.grid[x][y]
-      if department:
-        color = color_map.get(department.name, 'gray')  # Default gray for unknown departments
-        rect = plt.Rectangle((y * cell_width, facility.height - (x + 1) * cell_height),
-                             department.width * cell_width, department.height * cell_height, color=color)
-        ax.add_patch(rect)
+  areas = []
+  block = F_w
+  for dept in data:
+    areas.append(dept[0] * dept[1])
+    block = min(dept[0], block)
+    block = min(dept[1], block)
 
-  # Set axis limits and labels (optional)
-  ax.set_xlim(0, facility.width)
-  ax.set_ylim(0, facility.height)
-  ax.set_xlabel('X')
-  ax.set_ylabel('Y')
+  print(areas)
+  sweepwidth = 1
+  print(block)
+  
 
-  # Set title (optional)
-  ax.set_title('Facility Layout')
+  # F_w = 100
+  # F_l = 100
+  L = int(F_l / block)
+  W = int(F_w / block)
+  facility = [[0 for i in range(L)] for j in range(W)]
+  facility = np.array(facility)
+
+  start_w, start_l = 0, 0
+
+  for dept in deptlist:
+    # print("dept", dept)
+    # print("start:", start_w, start_l)
+    deptarea = areas[dept]
+
+    direction = start_l % 2
+
+    if not direction:
+      partial1 = (W - start_w)
+    else:
+      partial1 = start_w
+
+    deptarea -= partial1 * block
+    full = (deptarea // block) // W
+    partial = (deptarea // block) % W
+
+    # print(deptarea, partial1, full, partial)
+
+    if partial1 != 0:
+      if not direction:
+        for i in range(partial1):
+            facility[start_w + i, start_l] = dept + 1
+      else:
+        for i in range(partial1-1, -1, -1):
+          facility[i, start_l] = dept + 1
+
+      # print(facility, "printed partial1\n")
+      start_l += 1
+
+    # print(full, partial)
+
+    for j in range(full):
+      facility[:, start_l + j] = dept + 1
+      # print(facility, "printed full\n")
+
+    start_l += full
+    # print(full, partial)
+
+    if start_l % 2 == 0:
+      for i in range(partial):
+        facility[i, start_l] = dept + 1
+      start_w += partial
+    elif partial != 0:
+      for i in range(W-1, W-partial-1, -1):
+        facility[i, start_l] = dept + 1
+      # print(facility, "printed partial\n")
+      start_w += W-partial
+
+
+    start_w += partial1
+    start_w %= W
+  
+  print(facility)
+  # print(start_w, start_l)
+  
+  # # Create a color map for departments
+  # color_map = {'A': '#03071e', 'B': '#370617', 'C': '#6a040f','D':'#9d0208','E':'#d00000','F':'#dc2f02','G':'#e85d04','H':'#f48c06','I':'#faa307','J':'#ffba08'}
+
+  # # Create the visualization using Matplotlib
+  # fig, ax = plt.subplots()
+
+  # # Set cell size based on facility dimensions
+  # cell_width = 1
+  # cell_height = 1
+
+  # # Iterate through the grid and create rectangles for departments
+  # for x in range(facility.height):
+  #   for y in range(facility.width):
+  #     department = facility.grid[x][y]
+  #     if department:
+  #       color = color_map.get(department.name, 'gray')  # Default gray for unknown departments
+  #       rect = plt.Rectangle((y * cell_width, facility.height - (x + 1) * cell_height),
+  #                            department.width * cell_width, department.height * cell_height, color=color)
+  #       ax.add_patch(rect)
+
+  # # Set axis limits and labels (optional)
+  # ax.set_xlim(0, facility.width)
+  # ax.set_ylim(0, facility.height)
+  # ax.set_xlabel('X')
+  # ax.set_ylabel('Y')
+
+  # # Set title (optional)
+  # ax.set_title('Facility Layout')
+  # Importing required modules
+  plt.rcParams['figure.figsize']=(7,5)
+
+
+  plt.gca().invert_yaxis()
+  # Generating colormesh using pcolormesh() funcion
+  plt.pcolormesh(facility,cmap='GnBu')
+    
+  # Adding details to the plot    
+  plt.title('Facility Layout')
+  plt.xlabel('x-axis')
+  plt.ylabel('y-axis')
+  plt.legend()
+  # Displaying the plot
+  plt.show()
 
   # Save or display the image
   st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -170,7 +266,7 @@ facility = Facility(F_l, F_w)
 # departments = [L, W, adj]
 
 try:
-  output = ALDEP(facility, departments)
+  output = ALDEP(facility, data)
   print(output)
 except ValueError:
   st.warning("Insufficient Facility Dimensions")
